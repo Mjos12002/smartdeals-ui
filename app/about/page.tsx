351da1d920134cd9f1577cbbd90b0a563afbadab
Joseph <mjos12002@gmail.com>
2026-07-10 15:35:26 +0200

    Added contents on the about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,35 @@
 // About is used to display the about page of the application.
-export default function About(){
+export default function About() {
     return (
-        <div className="h-screen overflow-y-scroll flex justify-center items-center bg-[#F0F0F0] p-2">
-            <div className="bg-white w-full max-w-2xl h-screen rounded-lg shadow-md border border-[#C6C3C3]">
-                <h2>About us</h2>
-                <p>Welcome to our about page!</p>
+        <div className="overflow-y-scroll m-3 bg-[#F0F0F0] h-screen flex flex-col gap-4 justify-center border border-[#D9D9D9] lg:shadow-md lg:rounded-md p-2 lg:p-6">
+            
+            <div className="flex flex-col bg-white p-2 rounded-md border border-[#E8E8E8] shadow-md">
+                <p className="font-bold text-2xl text-[#004691] pb-2">
+                    About Smart Deals
+                </p>
+                <p className="text-justify">
+                    Is a web application that provides users with the best deals and discounts on various products and services.
+                </p>
             </div>
+
+            <div className="flex flex-col bg-white p-2 rounded-md border border-[#E8E8E8] shadow-md">
+                <p className="font-bold text-2xl text-[#004691] pb-2">
+                    Our mission
+                </p>
+                <p className="text-justify pb-10">
+                    Our mission is to help users <span className="font-bold">save money</span> while shopping online by providing them with the latest deals and offers from different retailers. We strive to make the shopping experience more enjoyable and affordable for our users.
+                </p>
+            </div>
+
+            <div className="flex flex-col bg-white p-2 rounded-md border border-[#E8E8E8] shadow-md">
+                <p className="font-bold text-2xl text-[#004691] pb-2">
+                    Our vision
+                </p>
+                <p className="text-justify">
+                    Our vision is to become the leading destination for shoppers looking for the best deals and discounts online. We aim to revolutionize the way people shop by providing them with a seamless and rewarding experience.
+                </p>
+            </div>
+
         </div>
     )
 }
