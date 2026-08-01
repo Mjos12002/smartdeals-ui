fda381bf8c19aa5f7de180ca5e5dc4172a587c62
Joseph <mjos12002@gmail.com>
2026-08-01 21:49:23 +0200

    Added product categories

--- app/component/product/product-category.component.tsx
+++ app/component/product/product-category.component.tsx
@@ -1,23 +1,42 @@
 import { getProductCategory } from "@/action/productcategory.action"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-// ProductCategoryComponent is used to display the product category
+interface IProductCategory {
+    name: string
+}
+
+// ProductCategoryComponent is used to render the product category
 export default function ProductCategoryComponent() {
+    
+    const [productCategories, setProductCategories] = useState<IProductCategory[]>([])
 
     useEffect(() => {
         getProductCategory()
         .then(data => {
             console.log(data.data)
+            if (data && data?.data != undefined) {
+                const productCategoryResponse: IProductCategory[] = data.data
+                setProductCategories(productCategoryResponse)
+            }else if (data?.data == undefined){
+                setProductCategories([])
+            }
         })
         .catch(err => console.log(err))
     }, [])
 
     return (
-        <div>
-            <ul>
-                <li>
+        <div className="flex items-center justify-center">
+            <ul className="flex gap-2 p-2">
+                <li className="font-bold text-[#00001F] bg-[#FAAC02] p-1 rounded-sm flex items-center">
                     All
                 </li>
+                {
+                    productCategories.map((category, index) => (
+                        <li key={index} className="flex items-center">
+                            {category.name}
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
