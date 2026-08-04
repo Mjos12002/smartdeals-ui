afd276224e9a4eb2e5bd1d575107c682d3cc7ba2
Joseph <mjos12002@gmail.com>
2026-08-04 12:53:55 +0200

    Added product category list on the index page

--- app/component/product/product-category.component.tsx
+++ app/component/product/product-category.component.tsx
@@ -13,7 +13,6 @@ export default function ProductCategoryComponent() {
     useEffect(() => {
         getProductCategory()
         .then(data => {
-            console.log(data.data)
             if (data && data?.data != undefined) {
                 const productCategoryResponse: IProductCategory[] = data.data
                 setProductCategories(productCategoryResponse)
@@ -21,18 +20,18 @@ export default function ProductCategoryComponent() {
                 setProductCategories([])
             }
         })
-        .catch(err => console.log(err))
+        .catch(err => setProductCategories([]))
     }, [])
 
     return (
-        <div className="flex items-center justify-center">
-            <ul className="flex gap-2 p-2">
-                <li className="font-bold text-[#00001F] bg-[#FAAC02] p-1 rounded-sm flex items-center">
+        <div className="flex items-center justify-center bg-[#000029] bg-gradient-to-r sm:from-[#000029] via-[#D3E6E0] to-[#00A324]">
+            <ul className="flex gap-2 m-2 overflow-x-scroll">
+                <li className="font-bold text-[#000] flex items-center p-1">
                     All
                 </li>
                 {
                     productCategories.map((category, index) => (
-                        <li key={index} className="flex items-center">
+                        <li key={index} className="flex items-center text-[#000] p-1 hover:bg-[#001204] hover:text-[#fff] p-1 cursor-pointer">
                             {category.name}
                         </li>
                     ))
--- app/page.tsx
+++ app/page.tsx
@@ -97,3 +97,4 @@ export default function Welcome() {
         </div>
     )
 }
+
